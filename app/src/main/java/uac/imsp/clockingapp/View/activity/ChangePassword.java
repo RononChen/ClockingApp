b10248899920873312e86b84e026777cea2aec43
Starias22 <adedeezechiel@gmail.com>
2022-08-27 17:48:00 +0100

    "Layout written for ChangePassword activity"

---
+++ app/src/main/java/uac/imsp/clockingapp/View/activity/ChangePassword.java
@@ -0,0 +1,14 @@
+package uac.imsp.clockingapp.View.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
