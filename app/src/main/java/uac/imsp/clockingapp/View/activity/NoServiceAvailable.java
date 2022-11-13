332f611813476f2c120edbae3e4740e89a6d7209
Starias22 <adedeezechiel@gmail.com>
2022-11-13 20:22:17 +0100

    "every is going to become OKAY"

---
+++ app/src/main/java/uac/imsp/clockingapp/View/activity/NoServiceAvailable.java
@@ -0,0 +1,16 @@
+package uac.imsp.clockingapp.View.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import uac.imsp.clockingapp.R;
+
+public class NoServiceAvailable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_service_available);
+    }
+}
