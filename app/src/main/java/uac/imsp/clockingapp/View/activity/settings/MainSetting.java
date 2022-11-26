4942c528b3f6553a3d17ef9e4eca5445bda0b598
Starias22 <adedeezechiel@gmail.com>
2022-11-26 04:21:50 +0100

    "main setting activity layout is created and general menu layout updated with setting icon"

---
+++ app/src/main/java/uac/imsp/clockingapp/View/activity/settings/MainSetting.java
@@ -0,0 +1,17 @@
+
+package uac.imsp.clockingapp.View.activity.settings;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import uac.imsp.clockingapp.R;
+
+public class MainSetting extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_setting);
+	}
+}
