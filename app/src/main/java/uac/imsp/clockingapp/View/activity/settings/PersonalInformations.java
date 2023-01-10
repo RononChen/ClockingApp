3b9a93f7506391c60a8a958a34e4e4ea3dabd3e1
Starias22 <adedeezechiel@gmail.com>
2023-01-10 11:55:36 +0100

    Back arrow added and handled in personal informations activity

--- app/src/main/java/uac/imsp/clockingapp/View/activity/settings/PersonalInformations.java
+++ app/src/main/java/uac/imsp/clockingapp/View/activity/settings/PersonalInformations.java
@@ -1,16 +1,37 @@
 package uac.imsp.clockingapp.View.activity.settings;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import uac.imsp.clockingapp.R;
 
 public class PersonalInformations extends AppCompatActivity {
 
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		if(item.getItemId()==android.R.id.home){
+			finish();
+			onBackPressed();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// calling the action bar
+		ActionBar actionBar = getSupportActionBar();
+// showing the back button in action bar
+		assert actionBar != null;
+		actionBar.setDisplayHomeAsUpEnabled(true);
 		setContentView(R.layout.activity_personal_informations);
 	}
+
+
 }
